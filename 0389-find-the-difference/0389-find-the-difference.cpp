034209d24d5a15f37eff75ae8eaf38c97a69c34f
AbdulRehman-github <143024733+AbdulRehman-github@users.noreply.github.com>
2024-06-11 18:44:06 +0500

    Time: 6 ms (21.54%), Space: 9.1 MB (18.96%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> mp;
+        for (char &ch : s) {
+            mp[ch]++;
+        }
+        for (char &ch : t) {
+            mp[ch]--;
+            if (mp[ch] < 0) {
+                return ch;
+            }
+        }
+        return 'x'; // This line should never be reached
+    }
+};
