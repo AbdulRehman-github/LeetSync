10ab1050ae16be624971e0fb42336825c9140fe3
AbdulRehman-github <143024733+AbdulRehman-github@users.noreply.github.com>
2024-06-25 18:29:41 +0500

    Time: 4 ms (59.18%), Space: 14.3 MB (7.65%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> arr;
+        unordered_map<int, int> um;
+        
+        for(auto num: nums1){
+         um[num]++;
+        }
+         for(auto num: nums2){
+             if(um[num] > 0){
+              arr.push_back(num);
+                 um[num]--;   
+             }
+         }
+        return arr;
+    }
+};
