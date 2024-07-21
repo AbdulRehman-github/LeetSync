c416d3bee46aeb0f9bf080dd5bd5e2b2fe55c2c3
AbdulRehman-github <143024733+AbdulRehman-github@users.noreply.github.com>
2024-07-21 16:21:14 +0500

    Time: 0 ms (100%), Space: 7.4 MB (37%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+     for(char &c : s){
+        if(c >= 'A' && c <= 'Z'){
+            c = c + ('a' - 'A');
+        }
+     }
+
+    return s;
+    }
+};
